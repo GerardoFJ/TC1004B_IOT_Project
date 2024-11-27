7e736f131e4f2781bbb39f104cb42b541a90a1c5
GerardoFJ <A00838645@tec.mx>
2024-11-27 01:20:22 -0600

    Micro_and_push

--- Arduino_codes/Push_Servo/Push_Servo.ino
+++ Arduino_codes/Push_Servo/Push_Servo.ino
@@ -1,18 +1,156 @@
 // 180 para cerrar
 // 0 para abrir
 #include <Servo.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h> 
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <ESP8266WebServer.h>
 
 #define SERVO_PIN 12
 #define BUTTON_PIN 13
 Servo servo;  // create servo object to control a servo  // variable to store the servo position
 
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "2.mx.pool.ntp.org", -21600, 60000);
+
+
+const char* ssid = "RoBorregos2";
+const char* password = "RoBorregos2024";
+
+
+
+HTTPClient httpClient;
+WiFiClient wClient;
+WiFiClientSecure httpsClient;
+
+
+const char *host = "fast-api-reto.onrender.com";
+const int httpsPort = 443;  //HTTPS= 443 and HTTP = 80
+const String Link = "/add-log-sensor";
+
+String device = "14";
+
+
+void start_ota_update(){
+  ArduinoOTA.setHostname("esp_pushservo");
+  ArduinoOTA.setPassword("esppush01");
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else {  // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+}
+
+
+void logAttempt(int data){
+  if(WiFi.status() == WL_CONNECTED){
+    String formattedDate = timeClient.getFormattedDate();
+
+    int splitT = formattedDate.indexOf("T");
+    String dayStamp = formattedDate.substring(0, splitT);
+    String timeStamp = formattedDate.substring(splitT+1, formattedDate.length()-1);
+    Serial.println(timeStamp);
+    String date_up = "\""+dayStamp+" "+timeStamp+"\"";
+    Serial.println(date_up);
+    String msg= "{\"date_\": "+date_up+",\"sensor_id\":"+device+",\"measure\":"+String(data)+"}";
+
+    httpsClient.setInsecure();
+    httpsClient.setTimeout(10000);
+
+    int r=0; //retry counter
+    while((!httpsClient.connect(host, httpsPort)) && (r < 30)){
+      delay(100);
+      Serial.print(".");
+      r++;
+  }
+
+  httpsClient.print(String("POST ") + Link + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Content-Type: application/json"+ "\r\n" +
+               "Content-Length:" + msg.length() + "\r\n\r\n" +
+               msg + "\r\n" +
+               "Connection: close\r\n\r\n");
+
+  }
+  Serial.println("Message sent");
+  return;
+}
+
 void setup() {
   Serial.begin(115200);
   servo.attach(SERVO_PIN);
-    // attaches the servo on pin D7 to the servo object
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  start_ota_update();
+    
+ WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi net \"");
+  Serial.print(ssid);
+  Serial.print("\"");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("\nConnected! IP: ");
+  Serial.println(WiFi.localIP());
+  timeClient.begin();
+  delay(500);
 }
 
 void loop() {
-                      // waits 10ms for the servo to reach the position
+  ArduinoOTA.handle();
+  timeClient.update();
+
+  int button_state = digitalRead(BUTTON_PIN);
+  if(button_state == LOW){
+    Serial.println("Button pressed");
+    servo.write(180);
+    delay(1000);
+  }
+  servo.write(0);
+
+  logAttempt(button_state);
+  ArduinoOTA.handle();
+  delay(1000);
+  ArduinoOTA.handle();
+
+  
 
 }
--- Arduino_codes/microphone_code/microphone_code.ino
+++ Arduino_codes/microphone_code/microphone_code.ino
@@ -1,51 +1,117 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
+#include <WiFiClientSecure.h> 
+#include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <ESP8266WebServer.h>
+
+
 
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP);
-const char* ssid = "Tec-IoT";
-const char* password = "spotless.magnetic.bridge";
+NTPClient timeClient(ntpUDP, "2.mx.pool.ntp.org", -21600, 60000);
+
+
+const char* ssid = "RoBorregos2";
+const char* password = "RoBorregos2024";
+
+
+
 HTTPClient httpClient;
 WiFiClient wClient;
-String URL = "http://fast-api-reto.onrender.com/add-log-sensor";
-const int micPin = 14;
+WiFiClientSecure httpsClient;
+
+
+const char *host = "fast-api-reto.onrender.com";
+const int httpsPort = 443;  //HTTPS= 443 and HTTP = 80
+const String Link = "/add-log-sensor";
+
 String device = "16";
 
-void logAttempt(int decibel){
+#define LED_1 13
+#define LED_2 15
+
+int prev_dato;
+
+void start_ota_update(){
+  ArduinoOTA.setHostname("esp_microphone");
+  ArduinoOTA.setPassword("espmicro01");
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else {  // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+}
+
+
+void logAttempt(int data){
   if(WiFi.status() == WL_CONNECTED){
-    
-    String postData;
-    String decibels;
-    decibels= String(decibel);
     String formattedDate = timeClient.getFormattedDate();
+
     int splitT = formattedDate.indexOf("T");
     String dayStamp = formattedDate.substring(0, splitT);
     String timeStamp = formattedDate.substring(splitT+1, formattedDate.length()-1);
     Serial.println(timeStamp);
     String date_up = "\""+dayStamp+" "+timeStamp+"\"";
     Serial.println(date_up);
-    postData= "{\"date_\": "+date_up+",\"sensor_id\":"+device+",\"measure\":"+decibel+"}";
-    Serial.print("Post Data String: ");
-    Serial.println(postData);
-    Serial.print("Host: ");
-    Serial.println(URL);
-    httpClient.begin(wClient, URL);
-    httpClient.addHeader("Content-Type", "application/json");
-    int httpCode = httpClient.POST(postData);
-    Serial.print("Response Code: ");
-    Serial.println(httpCode);
- 
-    httpClient.end();
+    String msg= "{\"date_\": "+date_up+",\"sensor_id\":"+device+",\"measure\":"+String(data)+"}";
+
+    httpsClient.setInsecure();
+    httpsClient.setTimeout(10000);
+
+    int r=0; //retry counter
+    while((!httpsClient.connect(host, httpsPort)) && (r < 30)){
+      delay(100);
+      Serial.print(".");
+      r++;
   }
+
+  httpsClient.print(String("POST ") + Link + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Content-Type: application/json"+ "\r\n" +
+               "Content-Length:" + msg.length() + "\r\n\r\n" +
+               msg + "\r\n" +
+               "Connection: close\r\n\r\n");
+
+  }
+  Serial.println("Message sent");
   return;
 }
 
 void setup(){
 Serial.begin(115200);
-pinMode(micPin, INPUT);
+pinMode(LED_1, OUTPUT);
+pinMode(LED_2, OUTPUT);
+start_ota_update();
  WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   delay(100);
@@ -62,13 +128,34 @@ pinMode(micPin, INPUT);
   Serial.println(WiFi.localIP());
   timeClient.begin();
   delay(500);
-
+  prev_dato= map(analogRead(A0), 500, 650, 0,100);
 }
 void loop(){
+ArduinoOTA.handle();
 timeClient.update();
 int dato = map(analogRead(A0), 500, 650, 0,100);
 Serial.println(dato);
-// logAttempt(dato);
-delay(200);
+if(prev_dato - dato > 5 || prev_dato - dato < -5){
+  Serial.println("Dato cambiado");
+  digitalWrite(LED_1, HIGH);
+  digitalWrite(LED_2, LOW);
+  delay(500);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, HIGH);
+  delay(500);
+  digitalWrite(LED_1, HIGH);
+  digitalWrite(LED_2, LOW);
+  delay(500);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, HIGH);
+  delay(500);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, LOW);
+}
+prev_dato =dato;
+logAttempt(dato);
+ArduinoOTA.handle();
+delay(1000);
+ArduinoOTA.handle();
 }
 
